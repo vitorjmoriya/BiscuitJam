ff3f9bbfdd159540b4fd6911451c06cb34a0c882
Bassani-Paulo <32801209+Bassani-Paulo@users.noreply.github.com>
2021-07-21 12:41:37 -0300

    Introduzindo o tocador de audio

---
+++ BiscuitJam/AudioManager/AudioPlayer.swift
@@ -0,0 +1,8 @@
+//
+//  AudioPlayer.swift
+//  BiscuitJam
+//
+//  Created by Paulo Bassani on 21/07/21.
+//
+
+import Foundation
--- BiscuitJam/Game/Components/Rat.swift
+++ BiscuitJam/Game/Components/Rat.swift
@@ -8,7 +8,7 @@
 import SpriteKit
 
 protocol Rat : SKSpriteNode {
-    
+    func move()
 }
 
 

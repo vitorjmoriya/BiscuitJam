76626b3ff64cdf1bcf5fe945bf25127b742704c4
Bassani-Paulo <32801209+Bassani-Paulo@users.noreply.github.com>
2021-07-26 10:37:14 -0300

    Player de Audio

--- BiscuitJam/AudioManager/AudioPlayer.swift
+++ BiscuitJam/AudioManager/AudioPlayer.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import AVFoundation
+
+class AudioManager {
+    public static var shared = AudioManager()
+    private var musicPlayer: AVAudioPlayer?
+    
+    private init() {
+        
+    }
+    
+    public func playMusic(name:String, musicExtension:String) {
+        if let musicURL = Bundle.main.url(forResource: name, withExtension: musicExtension){
+            do { musicPlayer = try AVAudioPlayer(contentsOf: musicURL)} catch{}
+            musicPlayer?.play()
+            musicPlayer?.numberOfLoops = -1
+        }
+    }
+    
+    public func playSoundEffect(name:String, soundExtension:String) {
+        var player = AVAudioPlayer()
+        if let soundURL = Bundle.main.url(forResource: name, withExtension: soundExtension){
+            do { player = try AVAudioPlayer(contentsOf: soundURL)} catch{}
+            player.play()
+        }
+    }
+}
